b5818e23ad9b72056cf104962a03320516d6faa9
Seo, Hyeong-Cheol <wjdqh6544@naver.com>
2024-12-09 21:48:51 +0900

    Clear unused file and Code Refactoring, Create Makefile
    - Clear unused file
    - Code Refactoring
    - Create Makefile

--- cleanup.h
+++
@@ -1,9 +0,0 @@
-#ifndef CLEANUP.H
-
-#define CLEANUP.H
-
-int cleanup_files_recursive(const char*, int, int);
-int cleanup_log_files(const char*, int, int);
-void log_deletion_record(const char*);
-
-#endif
